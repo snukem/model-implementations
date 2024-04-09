83ebb228ca6d186a9c9bf6b88e24809e0e007be9
Spencer <spencenewcomb08@gmail.com>
2024-04-09 13:57:38 -0700

    docstrings for KNN

--- python/KNNClassifier.py
+++ python/KNNClassifier.py
@@ -8,26 +8,52 @@ valid distance function. The relative scale of each feature matters.
 '''
 
 class KNNClassifier:
-    def __init__(self, k: int):
+    def __init__(self, k: int = 1):
+        """Constructor for KNNClassifier object.
+
+        Args:
+            k: Calculate distances to each of 'k' nearest neighbors in feature space.
+
+        Raises:
+            ValueError: If the provided 'k' is not positive.
+        """
+
         if k <= 0:
             raise ValueError("Tuning parameter 'k' must be a positive integer.")
+        
         self.k = k
-        pass
 
-    def fit(self, X_train, y_train, f: function = None):
+    def fit(self, X: np.array, y: np.array):
+        """Fitter method for KNNClassifier object.
+
+        Input data 'X' contains all predictive features for each of the training
+        observations. The response data 'y' contains encoded representation of the
+        member class of each training observation. These objects are stored respectively
+        as 'X_train' and 'y_train' to the classifier object.
+
+        Args:
+            X: (n by p) array of input data, consisting of 'p' features and 'n' 
+                observations.
+            y: (n by q) array of encoded responses, for 'q' output classes encoded to
+                0 (not a member of class) or 1 (member of class), and 'n' observations.
+
+        Raises:
+            ValueError: If tuning parameter 'k' is larger than number of observations.
+        """
+        self.X_train = X
+        self.y_train = y
         # if self.k > n(X_train) then raise ValueError
-
-        # fit the model
-
-        # if f then use it as user-defined distance function 
-        # else use f_dist
-
         pass
 
     def f_dist(x1, x2):
         # Euclidean distance between x1 and x2
         pass
 
-    def predict(self, X_test, y_test):
+    def predict(self, X_test, y_test, f: function = None):
+        # fit the model
+
+        # if f then use it as user-defined distance function 
+        # else use f_dist
+
         # Make a prediction for new observations
         pass
