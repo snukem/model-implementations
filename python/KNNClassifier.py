7e2ef747b1c0041fb56edf40d7b1247cb6adbc50
Spencer <spencenewcomb08@gmail.com>
2024-04-09 13:27:32 -0700

    KNN Description

--- python/KNNClassifier.py
+++ python/KNNClassifier.py
@@ -1,1 +1,7 @@
-# new
+'''
+Python implementation of the K-Nearest Neighbor (KNN) algorithm for classification.
+The KNN classifier predicts the class of a given test observation by identifying 
+training observations that are nearest to it in features space, as defined by some
+valid distance function. The relative scale of each feature matters. 
+'''
+
